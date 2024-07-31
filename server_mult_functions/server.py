4bde9537a4d408d5e467c2ea1abbbb9864b0379c
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2024-07-31 12:00:53 -0300

    Update server.py

--- server_mult_functions/server.py
+++ server_mult_functions/server.py
@@ -1,17 +1,22 @@
-import socket, threading, os
+import socket, threading, os, platform
 from server_services import *
+from network_services import *
 
 class Server(Server_Services_MixIn):
     DIRECTORY  = os.path.dirname(os.path.abspath(__file__))
-    DIRECTORY += '\\storage\\'
+    if platform.system() == 'Windows': DIRECTORY += '\\storage\\'
+    elif platform.system() == 'Linux': DIRECTORY += '/storage'
     FUNCTION_DICTIONARY = {
         "/?": Server_Services_MixIn.command_list(),
         "/f": Server_Services_MixIn.files_on_the_server(),
         "/d": Server_Services_MixIn.send_file_to_client(),
         "/u": Server_Services_MixIn.receive_file_from_client(),
         "/m": Server_Services_MixIn.private_message(),
-        "/b": Server_Services_MixIn(),
-        "/q": lambda Server: Server.remove_client_from_the_list()
+        "/b": Server_Services_MixIn.broadcast_message(),
+        "/q": lambda Server: Server.remove_client_from_the_list(),
+        "netcat": Network_Services_MixIn.netcat(),
+        "dns": Network_Services_MixIn.dns(),
+        "netscan": Network_Services_MixIn.netscan()
         }
 
     def __init__(self):
@@ -34,19 +39,23 @@ class Server(Server_Services_MixIn):
         while True:
             try:
                 message = (connection.recv(1024)).decode()
-                if message in Server.FUNCTION_DICTIONARY:
-                   result = Server.FUNCTION_DICTIONARY[message]
-                   Server.send_to_client(connection, result)
-                else: break
+                if message in Server.FUNCTION_DICTIONARY: 
+                    result = Server.FUNCTION_DICTIONARY[message]
+                else:
+                    result = 'Command not found'
+                Server.send_to_client(connection, result)
             except Exception as error:
+                print('ERROR INSIDE THE LOOP')
                 print(error)
-                self.remove_client_from_the_list(connection, client_address)
+                self.remove_client_from_the_list(self, connection, client_address)
 
     def add_client_to_the_list(self, connection, client_address):
         with self._lock:
             self._clients_list[client_address] = connection
 
-    def remove_client_from_the_list(self, connection, client_address):
+    def remove_client_from_the_list(self, *args):
+        connection     = args[0]
+        client_address = args[1]
         with self._lock:
             connection.close()
             del self._clients_list[client_address]
