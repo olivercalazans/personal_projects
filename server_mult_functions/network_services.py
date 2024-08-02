920337eacd3a64df9b72d45eff4ff38ebb3e25e7
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2024-08-02 16:24:51 -0300

    fix: renaming some variables

--- server_mult_functions/network_services.py
+++ server_mult_functions/network_services.py
@@ -7,32 +7,32 @@ class Network_Services_MixIn:
     def dns():
         ...
     
-    def netscan(strHost):
-        ipHost = socket.gethostbyname(strHost)
-        ports = { 21  : 'FTP - File Transfer Protocol',  
-                  22  : 'SSH - Secure Shell',  
-                  23  : 'Telnet',  
-                  25  : 'SMTP - Simple Mail Transfer Protocol',   
-                  53  : 'DNS - Domain Name System', 
-                  80  : 'HTTP - HyperText Transfer Protocol', 
-                  110 : 'POP3 - Post Office Protocol version 3', 
-                  443 : 'HTTPS - HTTP Protocol over TLS/SSL', 
-                  5432: 'PostgreSQL database system', 
-                  8080: 'Jakarta Tomcat'
-                  }  
+    def portscan(_host):
+        _ip = socket.gethostbyname(_host)
+        _ports = { 21  : 'FTP - File Transfer Protocol',  
+                   22  : 'SSH - Secure Shell',  
+                   23  : 'Telnet',  
+                   25  : 'SMTP - Simple Mail Transfer Protocol',   
+                   53  : 'DNS - Domain Name System', 
+                   80  : 'HTTP - HyperText Transfer Protocol', 
+                   110 : 'POP3 - Post Office Protocol version 3', 
+                   443 : 'HTTPS - HTTP Protocol over TLS/SSL', 
+                   5432: 'PostgreSQL database system', 
+                   8080: 'Jakarta Tomcat'
+                  }
         try:
-            data = list()
-            for port in ports.keys():
+            _data = list()
+            for port in _ports.keys():
                 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 sock.settimeout(3) 
-                result = sock.connect_ex((ipHost, port))
+                result = sock.connect_ex((_ip, port))
                 status = 'Closed'
                 if result == 0: status = 'Opened'
-                data.append(f' Port {port:>4} : {ports[port]} (STATUS -> {status})')
+                _data.append(f' Port {port:>4} : {_ports[port]} (STATUS -> {status})')
                 sock.close()
-            return data
+            return _data
         except socket.gaierror:
-           return 'ERROR: problems with DNS'
+            return 'ERROR: problems with DNS'
         except socket.error:
-           return 'ERROR: It was not possible to connect to the server'
+            return 'ERROR: It was not possible to connect to the server'
     
