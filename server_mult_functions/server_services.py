b9e1f0cdf94d6216d7e07c50b07c85709d029853
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2024-08-02 16:23:15 -0300

    Moving some functions and creating a new one

--- server_mult_functions/server_services.py
+++ server_mult_functions/server_services.py
@@ -1,20 +1,4 @@
 class Server_Services_MixIn:
-    def convert_to_string(data):
-        return '|'.join(data)
-
-    def command_list():
-        commands = ['/f - Files on the server',
-                    '/d - Download from the server',
-                    '/u - Upload to the server',
-                    '/m - Private message',
-                    '/b - Broadcast message',
-                    '/q - Log out',
-                    '/netcat'
-                    'dns',
-                    'netscan'
-                    ]
-        return Server_Services_MixIn.convert_to_string(commands)
-    
     def files_on_the_server():
         return "Command not avaliable yet"
     
@@ -23,10 +7,6 @@ class Server_Services_MixIn:
     
     def receive_file_from_client():
         return "Command not avaliable yet"
-
-    def private_message():
-        return "Command not avaliable yet"
     
-    def broadcast_message():
+    def delete_file():
         return "Command not avaliable yet"
-    
