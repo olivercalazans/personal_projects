9a7e2411980bd95ed968df995b82089d9f479b3b
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2024-08-06 17:33:51 -0300

    fix: change of some variable names and add of a tag.

--- server_mult_functions/server.py
+++ server_mult_functions/server.py
@@ -5,23 +5,23 @@ from network_services import *
 class Server(Server_Services_MixIn, Network_Services_MixIn):
     DIRECTORY = os.path.dirname(os.path.abspath(__file__))
 
-    if platform.system() == 'Windows': DIRECTORY += '\\storage\\'
-    elif platform.system() == 'Linux': DIRECTORY += '/storage'
+    if platform.system() == 'Windows': DIRECTORY += '\\server_folder\\'
+    elif platform.system() == 'Linux': DIRECTORY += '/server_folder'
 
     FUNCTION_DICTIONARY = {
-        "/?":        lambda self, args=None: self.command_list(),
-        "/exit":     lambda self, args=None: self.remove_client_from_the_list(),
-        "/files":    lambda self, args=None: self.files_on_the_server(),
-        "/downl":    lambda self, args=None: self.send_file_to_client(),
-        "/upl":      lambda self, args=None: self.receive_file_from_client(),
-        "/msg":      lambda self, args=None: self.private_message(),
-        "/bmsg":     lambda self, args=None: self.broadcast_message(),
-        "/netcat":   lambda self, args=None: self.netcat(),
-        "/dns":      lambda self, args=None: self.dns(),
-        "/portscan": lambda self, args=None: self.portscan(args) if args else 'No host specified'
+        "/?":        lambda self, arguments=None: self.command_list(),
+        "/exit":     lambda self, arguments=None: self.remove_client_from_the_list(),
+        "/files":    lambda self, arguments=None: self.files_on_the_server(),
+        "/downl":    lambda self, arguments=None: self.send_file_to_client(),
+        "/upl":      lambda self, arguments=None: self.receive_file_from_client(),
+        "/msg":      lambda self, arguments=None: self.private_message(),
+        "/bmsg":     lambda self, arguments=None: self.broadcast_message(),
+        "/netcat":   lambda self, arguments=None: self.netcat(),
+        "/dns":      lambda self, arguments=None: self.dns(),
+        "/portscan": lambda self, arguments=None: self.portscan(arguments) if arguments else 'No host specified'
     }
 
-    def __init__(self):
+    def __init__(self) -> None:
         self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._server_socket.bind(('localhost', 10000))
         self._server_socket.listen(4)
@@ -31,34 +31,34 @@ class Server(Server_Services_MixIn, Network_Services_MixIn):
         Server.create_directory(Server.DIRECTORY)
 
     @staticmethod
-    def create_directory(_directory):
+    def create_directory(_directory) -> None:
         try:   os.mkdir(_directory)
         except FileExistsError: print('The directory already exists')
         except Exception as error: print(f'Error creating directory: {error}')
         else:  print('Directory created')
 
     @staticmethod
-    def separating_function_from_arguments(_string):
-        _function_key, _arguments = (_string.split(':', 1) + [None])[:2]
-        return _function_key, _arguments
+    def separating_function_from_arguments(_string) -> list:
+        _function_key, _args = (_string.split(':', 1) + [None])[:2]
+        return _function_key, _args
 
     @staticmethod
-    def send_to_client(connection, result):
+    def send_to_client(connection, result) -> None:
         connection.sendall(result.encode())
 
-    def receive_client(self):
+    def receive_client(self) -> None:
         while True:
             connection, client_address = self._server_socket.accept()
             print(f'New log in: {client_address}')
             threading.Thread(target=self.handle_client, args=(connection, client_address)).start()
 
-    def handle_client(self, connection, client_address):
+    def handle_client(self, connection, client_address) -> None:
         self.add_client_to_the_list(connection, client_address)
         while True:
             try:
-                _function, _args = Server.separating_function_from_arguments((connection.recv(1024)).decode())
+                _function, _arguments = Server.separating_function_from_arguments((connection.recv(1024)).decode())
                 if _function in Server.FUNCTION_DICTIONARY:
-                    _result = Server.FUNCTION_DICTIONARY[_function](self, _args)
+                    _result = Server.FUNCTION_DICTIONARY[_function](self, _arguments)
                 else:
                     _result = 'Command not found'
                 Server.send_to_client(connection, _result)
@@ -67,22 +67,22 @@ class Server(Server_Services_MixIn, Network_Services_MixIn):
                 print(error)
                 self.remove_client_from_the_list(connection, client_address)
 
-    def add_client_to_the_list(self, connection, client_address):
+    def add_client_to_the_list(self, connection, client_address) -> None:
         with self._lock:
             self._clients_list[client_address] = connection
 
-    def remove_client_from_the_list(self, connection, client_address):
+    def remove_client_from_the_list(self, connection, client_address) -> None:
         with self._lock:
             connection.close()
             del self._clients_list[client_address]
 
-    def private_message(self):
+    def private_message(self) -> None:
         return "Command not available yet"
     
-    def broadcast_message(self):
+    def broadcast_message(self) -> None:
         return "Command not available yet"
 
-    def command_list(self):
+    def command_list(self) -> str:
         commands = [
             '/files - Files on the server',
             '/downl - Download from the server',
@@ -97,7 +97,7 @@ class Server(Server_Services_MixIn, Network_Services_MixIn):
         return Server.convert_to_string(commands)
 
     @staticmethod
-    def convert_to_string(_data):
+    def convert_to_string(_data) -> str:
         return '|'.join(_data)
 
 if __name__ == '__main__':
